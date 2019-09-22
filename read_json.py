6dbc24574d2b46234d1f67f8443f9cd3beb8bf11
zhoukaisheng <751296218@qq.com>
2019-09-22 18:08:37 +0800

    /

---
+++ read_json.py
@@ -0,0 +1,12 @@
+import json
+with open('C:/Users/75129/Desktop/nni实验记录/mixed_1d(16)-0.8350.json','r') as f:
+    line = f.read()
+    d = json.loads(line)
+    # data = d['trialMessage']['finalMetricData']['data']
+auc=[]
+for i in range(100):
+    try:
+        auc.append(d['trialMessage'][i]['finalMetricData'][0]['data'])
+    except KeyError as identifier:
+        auc.append(d['trialMessage'][i]['intermediate'][-1]['data'])
+print(max(auc))
