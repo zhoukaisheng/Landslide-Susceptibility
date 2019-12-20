ea7d01b4077f378d6f976979936d7c636d1e7ab3
zhoukaisheng <751296218@qq.com>
2019-12-20 22:41:59 +0800

    save memory but huge disk use

--- nni_trial_mix/mixture_cnn.py
+++ nni_trial_mix/mixture_cnn.py
@@ -42,56 +42,29 @@ import nni
 import sys
 sys.path.append('../')
 import data_read
+import read_pixel_data
+import imageio
 
+np.random.seed(6)
 
 LOG = logging.getLogger('ayf_nni_cnn')
 #K.set_image_data_format('channels_last')
 TENSORBOARD_DIR = os.environ['NNI_OUTPUT_DIR']
 print(TENSORBOARD_DIR)
 
-H, W = 40, 40
-CHANNELS=16
+
 NUM_CLASSES = 2
 
-def create_model(hyper_params, input_shape=(H, W, CHANNELS), num_classes=NUM_CLASSES):
-    '''
-    Create simple convolutional model
-    '''
-    
-    layers = [
-        BatchNormalization(axis=3, input_shape=input_shape),
-        Conv2D(np.int32(hyper_params['Conv2D_1_number']), kernel_size=(np.int32(hyper_params['kernel_size']), np.int32(hyper_params['kernel_size'])), activation=hyper_params['activation']),
-        Conv2D(np.int32(hyper_params['Conv2D_2_number']), kernel_size=(np.int32(hyper_params['kernel_size']),np.int32(hyper_params['kernel_size'])), activation=hyper_params['activation'],),
-        MaxPooling2D(pool_size=(np.int32(hyper_params['pool_size']), np.int32(hyper_params['pool_size']))),
-        Flatten(),
-        Dense(np.int32(hyper_params['dense_size']), activation=hyper_params['activation'],),
-        Dropout(hyper_params['Dropout_rate']),
-        Dense(num_classes, activation='softmax')
-    ]
-
-    model = Sequential(layers)
-    
-    if hyper_params['optimizer'] == 'Adam':
-        optimizer = op.Adam(lr=hyper_params['learning_rate'])
-    else:
-        optimizer = op.SGD(lr=hyper_params['learning_rate'], momentum=0.9)
-    model.compile(loss=losses.categorical_crossentropy, optimizer=optimizer, metrics=['accuracy'])
-
-    return model
-
-def create_3d_model(inputs,hyper_params):
+def create_3d_model(inputs,hyper_params,H,W,CHANNELS):
     # inputs=Input(shape=(40,40,16))
     x=BatchNormalization()(inputs)
-    x=DepthwiseConv2D(kernel_size=(3,3),activation='relu')(x)
+    x=DepthwiseConv2D(kernel_size=(3,3),activation='relu',padding='same')(x)
     x=MaxPooling2D(pool_size=(3,3))(x)
-    x=DepthwiseConv2D(kernel_size=(3,3),activation='relu')(x)
-    x=MaxPooling2D(pool_size=(3,3))(x)
-    x=DepthwiseConv2D(kernel_size=(3,3),activation='relu')(x)
     x=Flatten()(x)
     return x
 
 def create_mixture_model(hyper_params):
-    inputs_3d=Input(shape=(40,40,16))
+    inputs_3d=Input(shape=(H,W,CHANNELS))
     x_3d=create_3d_model(inputs_3d,hyper_params)
     x_1d=Input(shape=(4,))
     mixtured=merge.concatenate([x_3d,x_1d])
@@ -108,9 +81,9 @@ def create_mixture_model(hyper_params):
     model.summary()
     return model
 
-def create_Only3D_model(hyper_params):
-    inputs_3d=Input(shape=(40,40,16))
-    x_3d=create_3d_model(inputs_3d,hyper_params)
+def create_Only3D_model(hyper_params,H,W,CHANNELS):
+    inputs_3d=Input(shape=(H,W,CHANNELS))
+    x_3d=create_3d_model(inputs_3d,hyper_params,H,W,CHANNELS)
     # x_1d=Input(shape=(20,))
     # mixtured=merge.concatenate([x_3d,x_1d])
     z=BatchNormalization()(x_3d)
@@ -190,13 +163,12 @@ def train(args, params):
     '''
     Train model
     '''
-    data=data_read.yongxin_data()
-    train_x3d,train_y,test_x3d,test_y=data.get_train_data(tr_path='../data/yongxin/tr_index.npy',
-                                                      tt_path='../data/yongxin/tt_index.npy',
-                                                      data_type='3D')
-    train_x1d,train_y,test_x1d,test_y=data.get_train_data(tr_path='../data/yongxin/tr_index.npy',
-                                                      tt_path='../data/yongxin/tt_index.npy',
-                                                      data_type='1D')                                    
+    
+    data=data_read.from_a_c_data(a=params['a'],c=params['c'])
+    train_x3d,train_y,test_x3d,test_y=data.get_train_data(data_type='3D')  
+    H=train_x3d.shape[1]
+    W=train_x3d.shape[2]
+    CHANNELS=16                   
 #    data_aug_generator=data_read.data_aug(train_x,train_y)
 #    rot_x,rot_y=data_aug_generator.rotate(train_x,train_y)
 #    flip_x,flip_y=data_aug_generator.flip(train_x,train_y)
@@ -205,11 +177,45 @@ def train(args, params):
 #    train_x,train_y=aug_data
     train_y=data_read.label_to_onehot(train_y)
     test_y=data_read.label_to_onehot(test_y)
-    model = create_mixture_model(params)
+    model = create_Only3D_model(params,H=H,W=W,CHANNELS=CHANNELS)
     # train_x3d=np.expand_dims(train_x3d,axis=5)
     # test_x3d=np.expand_dims(test_x3d,axis=5)
-    train_data=[train_x3d,train_x1d[:,-4:]]
-    test_data=[test_x3d,test_x1d[:,-4:]]
+    train_data=train_x3d
+    test_data=test_x3d
+    SendMetric=SendMetrics(validation_data=(test_data,test_y))
+    model.fit(train_data, train_y,  epochs=args.epochs, verbose=1,
+        validation_data=(test_data, test_y), callbacks=[SendMetric, TensorBoard(log_dir=TENSORBOARD_DIR)])
+    y_pred=model.predict(test_data)
+    score = roc_auc_score(test_y[:,1], y_pred[:,1])
+#    _, acc = model.evaluate(x_test, y_test, verbose=0)
+    LOG.debug('Final result is: %d', score)
+    nni.report_final_result(score)
+
+def pixel_train(args, params):
+    '''
+    Train model with pixel
+    '''
+    factors=read_pixel_data.read_factor_data()
+    factors_3d=read_pixel_data.get_3d_x(factors,params['window_size'])
+    label=imageio.imread('D:/yanshan_new/tr_and_tt.tif')
+    train_x3d,train_y,test_x3d,test_y=read_pixel_data.get_tr_tt_data_3d(factors_3d,label)
+    # train_x3d,train_y,test_x3d,test_y=data.get_train_data(data_type='3D')  
+    H=train_x3d.shape[1]
+    W=train_x3d.shape[2]
+    CHANNELS=16                   
+#    data_aug_generator=data_read.data_aug(train_x,train_y)
+#    rot_x,rot_y=data_aug_generator.rotate(train_x,train_y)
+#    flip_x,flip_y=data_aug_generator.flip(train_x,train_y)
+#    noise_x,noise_y=data_aug_generator.add_noise(train_x,train_y,sigma=0.5)
+#    aug_data=get_mixup_data(train_x,train_y,[flip_x,flip_y],[rot_x,rot_y],[noise_x,noise_y],params)
+#    train_x,train_y=aug_data
+    train_y=data_read.label_to_onehot(train_y)
+    test_y=data_read.label_to_onehot(test_y)
+    model = create_Only3D_model(params,H=H,W=W,CHANNELS=CHANNELS)
+    # train_x3d=np.expand_dims(train_x3d,axis=5)
+    # test_x3d=np.expand_dims(test_x3d,axis=5)
+    train_data=train_x3d
+    test_data=test_x3d
     SendMetric=SendMetrics(validation_data=(test_data,test_y))
     model.fit(train_data, train_y,  epochs=args.epochs, verbose=1,
         validation_data=(test_data, test_y), callbacks=[SendMetric, TensorBoard(log_dir=TENSORBOARD_DIR)])
@@ -244,7 +250,7 @@ if __name__ == '__main__':
         PARAMS = generate_default_params()
         PARAMS.update(RECEIVED_PARAMS)
         # train
-        train(ARGS, PARAMS)
+        pixel_train(ARGS, PARAMS)
     except Exception as e:
         LOG.exception(e)
         raise
--- read_pixel_data.py
+++ read_pixel_data.py
@@ -37,6 +37,56 @@ def read_factor_data():
         i = i+1
     # pca = PCA(n_c
     return img
+def get_3d_x_save_memory(factors,window_size):
+    # windows_size must be Odd number
+    factors_3d=np.zeros([factors.shape[0],factors.shape[1],factors.shape[2]])
+    index=np.arange(0,window_size*window_size)
+    index=index.reshape([window_size,window_size])
+    # print(index)
+    loc_x=[]
+    loc_y=[]
+    r=(window_size-1)/2
+    for i in range(window_size*window_size):
+        loc=np.where(index==i)
+        x=loc[0]
+        y=loc[1]
+        x=r-x
+        y=r-y
+        loc_x.append(x)
+        loc_y.append(y)
+    # print(loc_x)
+    # print(loc_y)
+    for i in range(window_size*window_size):
+        print(i)
+        x_start=int(max(loc_x[i],0))
+        x_end=int(min(loc_x[i],-0))
+        y_start=int(max(loc_y[i],0))
+        y_end=int(min(loc_y[i],-0))
+
+        x_start_3d=-x_end
+        x_end_3d=-x_start
+        y_start_3d=-y_end
+        y_end_3d=-y_start
+        # print(x_start,x_end)
+        # print(y_start,y_end)
+        if x_end==0:
+            x_end=None
+        if y_end==0:
+            y_end=None
+        if x_end_3d==0:
+            x_end_3d=None 
+        if y_end_3d==0:
+            y_end_3d=None 
+
+        tmp=factors[x_start_3d:x_end_3d,y_start_3d:y_end_3d,:]
+        # print(tmp.shape)
+        # print(factors_3d[i,x_start_3d:x_end_3d,y_start_3d:y_end_3d,:].shape)
+        factors_3d[x_start:x_end,y_start:y_end,:]=tmp
+        np.save('D:/yanshan_new/pixel_traing_data/factors3d_'+str(window_size)+'_'+str(i)+'.npy',factors_3d)
+    # factors_3d=factors_3d.reshape([factors.shape[0],factors.shape[1],window_size,window_size,factors.shape[2]])
+        print(factors_3d.shape)
+    return None
+
 def get_3d_x(factors,window_size):
     # windows_size must be Odd number
     factors_3d=np.zeros([factors.shape[0],factors.shape[1],window_size*window_size,factors.shape[2]])
@@ -112,15 +162,82 @@ def get_tr_tt_data_3d(data_3d,tr_tt_img):
         elif i==3:
             tr_x=np.concatenate((tr_x,tmp_data),axis=0)
             tr_y=np.concatenate((tr_y,np.ones([tmp_data.shape[0],])),axis=0)
-    print(tt_x.shape)
-    print(tt_y)
+    # print(tt_x.shape)
+    # print(tt_y)
     return tr_x,tr_y,tt_x,tt_y
+
+def get_tr_tt_data_3d_save_memory(data_3d_path,tr_tt_img,window_size):
+    #1 tt_nolandslide 0
+    #2 tr_nolandslide 1
+    #3 tt_landslide 2
+    #4 tr_landslide 3
+    #data_3d.shape = (row,col,channel)
+    train_x=[]
+    # train_y=[]
+    test_x=[]
+    # test_y=[]
+    tr_tt_img=tr_tt_img.reshape([tr_tt_img.shape[0]*tr_tt_img.shape[1],])
+    locs=get_row_col(tr_tt_img)
+    for i in range(window_size*window_size):
+        data_3d=np.load(data_3d_path+'factors3d_'+str(window_size)+'_'+str(i)+'.npy')
+        # data_3d=np.load('D:/yanshan_new/pixel_traing_data/factors3d_'+str(window_size)+'_'+str(i)+'.npy')
+        data_3d=data_3d.reshape([data_3d.shape[0]*data_3d.shape[1],data_3d.shape[2]])
+        
+        
+        for i in range(4):
+            loc=locs[i]
+            # print(loc)
+            tmp_data=data_3d[loc,:]
+            tmp_data=np.squeeze(tmp_data)
+            # print(tmp_data.shape)
+            if i==0:
+                tt_x=tmp_data
+                tt_y=np.zeros([tmp_data.shape[0],])
+            elif i==1:
+                tr_x=tmp_data
+                tr_y=np.zeros([tmp_data.shape[0],])
+            elif i==2:
+                tt_x=np.concatenate((tt_x,tmp_data),axis=0)
+                tt_y=np.concatenate((tt_y,np.ones([tmp_data.shape[0],])),axis=0)
+            elif i==3:
+                tr_x=np.concatenate((tr_x,tmp_data),axis=0)
+                tr_y=np.concatenate((tr_y,np.ones([tmp_data.shape[0],])),axis=0)
+        train_x.append(tr_x)
+        test_x.append(tt_x)
+    train_x=np.array(train_x)
+    test_x=np.array(test_x)
+
+    train_x=np.swapaxes(train_x,0,1)
+    test_x=np.swapaxes(test_x,0,1)
+
+    train_x=train_x.reshape([train_x.shape[0],window_size,window_size,train_x.shape[2]])
+    test_x=test_x.reshape([test_x.shape[0],window_size,window_size,test_x.shape[2]])
+    print(train_x.shape)
+    print(test_x.shape)
+    plt.imshow(train_x[10,:,:,0])
+    plt.show()
+    # print(tt_x.shape)
+    # print(tt_y)
+    return train_x,tr_y,test_x,tt_y
+
 
 
 
 if __name__ == '__main__':
     factors=read_factor_data()
-    factors_3d=get_3d_x(factors,3)
+    window_size=9
+    print('begin get_3d_x')
+    factors_3d=get_3d_x_save_memory(factors,window_size)
+    print('end get_3d_x')
     label=imageio.imread('D:/yanshan_new/tr_and_tt.tif')
-    tr_x,tr_y,tt_x,tt_y=get_tr_tt_data_3d(factors_3d,label)
+    data_3d_path='D:/yanshan_new/pixel_traing_data/'
+    tr_x,tr_y,tt_x,tt_y=get_tr_tt_data_3d_save_memory(data_3d_path,label,window_size)
+    # tr_x,tr_y,tt_x,tt_y=get_tr_tt_data_3d(factors_3d,label)
+    # save memory
+    # np.save('D:/yanshan_new/pixel_traing_data/factors_3d_'+str(window_size)+'.npy',factors_3d)
+    np.save('D:/yanshan_new/pixel_traing_data/tr_x_'+str(window_size)+'.npy',tr_x)
+    np.save('D:/yanshan_new/pixel_traing_data/tr_y_'+str(window_size)+'.npy',tr_y)
+    np.save('D:/yanshan_new/pixel_traing_data/tt_x_'+str(window_size)+'.npy',tt_x)
+    np.save('D:/yanshan_new/pixel_traing_data/tt_y_'+str(window_size)+'.npy',tt_y)
+
 
